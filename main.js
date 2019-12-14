99ed59c67e21d1c50e4f94c52989b50e81f39566
rmamchyk <roman.mamchyk@gmail.com>
2019-12-14 14:19:53 +0200

    initial commit

---
+++ main.js
@@ -0,0 +1,28 @@
+const {app, BrowserWindow} = require('electron')
+
+let mainWindow
+
+function createWindow () {
+  mainWindow = new BrowserWindow({
+    width: 1000, height: 800,
+    webPreferences: { nodeIntegration: true }
+  })
+
+  mainWindow.loadFile('index.html')
+  // mainWindow.webContents.openDevTools();
+
+  mainWindow.on('closed',  () => {
+    mainWindow = null
+  })
+}
+
+app.on('ready', createWindow)
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') app.quit()
+})
+
+// When app icon is clicked and app is running, (macOS) recreate the BrowserWindow
+app.on('activate', () => {
+  if (mainWindow === null) createWindow()
+})
